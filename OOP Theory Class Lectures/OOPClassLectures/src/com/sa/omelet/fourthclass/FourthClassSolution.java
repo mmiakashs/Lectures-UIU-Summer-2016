6212c6c8926e12863013d89541f913a9e82a1173
MD. MOFIJUL ISLAM AKASH <akash.cse.du@gmail.com>
2016-06-08 20:38:13 +0600

    OOP theory fourth lectures code added

---
+++ OOP Theory Class Lectures/OOPClassLectures/src/com/sa/omelet/fourthclass/FourthClassSolution.java
@@ -0,0 +1,27 @@
+package com.sa.omelet.fourthclass;
+
+public class FourthClassSolution {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student1 = new Student();
+        student1.setStudentName("Anik");
+
+        Student student2 = new Student("Safat", "0154125");
+
+        System.out.println("Before Swap");
+        System.out.println(student1.getStudentName());
+        System.out.println(student2.getStudentName());
+
+        swap(student1);
+        System.out.println("After Swap");
+        System.out.println(student1.getStudentName());
+        System.out.println(student2.getStudentName());
+
+    }
+
+    public static void swap(Student st1){
+        st1.setStudentName("Noman");
+
+    }
+}
---
+++ OOP Theory Class Lectures/OOPClassLectures/src/com/sa/omelet/fourthclass/Student.java
@@ -0,0 +1,39 @@
+package com.sa.omelet.fourthclass;
+
+/**
+ * Created by akashs on 6/7/16.
+ */
+public class Student {
+    private String studentName;
+    private String studentId;
+
+    public Student(){
+        System.out.println("Hello Constructor");
+    }
+
+    public Student(String studentname, String studentid) {
+        studentName = studentname;
+        studentId = studentid;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    protected void finalize() {
+        System.out.println(studentName+" die");
+    }
+}
